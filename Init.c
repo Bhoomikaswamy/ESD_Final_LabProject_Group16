a374ddcf7352c3aab4a2fcb2259eaff5483b0cd4
Bhoomikaswamy <mbhoomikaswamy@gmail.com>
2024-11-28 23:07:39 +0530

    Updating main.c file

--- Init.c
+++ Init.c
@@ -1,115 +1,61 @@
-/*
- * Init.c
- *
- *  Created on: 27-Nov-2024
- *      Author: Bhoomika Swamy
- */
-
+#include "tm4c123gh6pm.h"
 #include <stdint.h>
 #include <stdbool.h>
-#include "tm4c123gh6pm.h"
-#include "definitions.h"
+#include "init.h"
 
-void GPIO_PortA_Initialisation(void){
+// UART0 Initialization
+void UART0_Init(void) {
+    SYSCTL_RCGCUART_R |= 0x01;        // Enable clock for UART0
+    SYSCTL_RCGCGPIO_R |= 0x01;        // Enable clock for GPIO Port A
 
-// SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
+    while ((SYSCTL_PRUART_R & 0x01) == 0); // Wait for UART0 ready
 
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;// Enable clock for Port A
-    while ((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R0) == 0);          // Allow time for clock to stabilize
-    GPIO_PORTA_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port A
-    GPIO_PORTA_CR_R |= 0x3c;                 // Commit changes,1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_DEN_R |= 0x3c;                // Digital function enable, 1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_DIR_R |= 0x2c;                // Set output/input, 1-output (PA7-PA0 = 00101100)
-    GPIO_PORTA_PUR_R |= 0x0c;                // Enable pull-up resistor, 1-enable (PA7-PA0 = 00001100)
-    GPIO_PORTA_DATA_R = 0x00;               // Reset the data register (PA7-PA0 = 00000000)
+    UART0_CTL_R &= ~0x01;             // Disable UART0 during configuration
+    UART0_IBRD_R = 104;               // Integer part of BRD (16MHz/9600)
+    UART0_FBRD_R = 11;                // Fractional part of BRD
+    UART0_LCRH_R = 0x70;              // 8-bit, no parity, 1-stop bit
+    UART0_CC_R = 0x0;                 // Use system clock
+    UART0_CTL_R = 0x301;              // Enable UART0, TXE, RXE
 
+    GPIO_PORTA_AFSEL_R |= 0x03;       // Enable alternate function for PA0, PA1
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00) | 0x00000011;
+    GPIO_PORTA_DEN_R |= 0x03;         // Enable digital function for PA0, PA1
 }
 
-void GPIO_PortB_Initialisation(void){
-
-// SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
-
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;// Enable clock for Port B
-    while ((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R1) == 0);          // Allow time for clock to stabilize
-    GPIO_PORTB_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port B
-    GPIO_PORTB_CR_R |= 0xf0;                 // Commit changes,1-enable (PB7-PB0 = 11110000)
-    GPIO_PORTB_DEN_R |= 0xf0;                // Digital function enable, 1-enable (PB7-PB0 =11110000)
-    GPIO_PORTB_DIR_R |= 0xb0;                // Set output/input, 1-output (PB7-PB0 = 10110000)
-    GPIO_PORTB_PUR_R |= 0x30;                // Enable pull-up resistor, 1-enable (PB7-PB0 = 00110000)
-    GPIO_PORTB_DATA_R = 0x00;               // Reset the data register (PB7-PB0 = 00000000)
-
+// UART send string
+void UART0_SendString(const char* str) {
+    while (*str) {
+        while ((UART0_FR_R & 0x20) != 0); // Wait until TX buffer is not full
+        UART0_DR_R = *str++;
+    }
 }
 
-void GPIO_PortF_Initialisation(void){
+// SPI Initialization
+void SPI_Init(void) {
+    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;  // Enable clock for SSI0
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0; // Enable clock for GPIO Port A
 
-// PORTF, PF7-PF0, PF4-SW1, PF3-green, PF2-blue, PF1-red, PF0-SW2
+    while ((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R0) == 0); // Wait for stabilization
 
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;// Enable clock for Port F
-    while ((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R5) == 0);          // Allow time for clock to stabilize
-    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port F
-    GPIO_PORTF_CR_R |= 0x1f;                 // Commit changes,1-enable (PF7-PF0 = 00011111)
-    GPIO_PORTF_DEN_R |= 0x1f;                // Digital function enable, 1-enable (PF7-PF0 = 00011111)
-    GPIO_PORTF_DIR_R |= 0x0e;                // Set output/input, 1-output (PF7-PF0 = 00001110)
-    GPIO_PORTF_PUR_R |= 0x11;                // Enable pull-up resistor, 1-enable (PF7-PF0 = 00010001)
-    GPIO_PORTF_DATA_R = 0x00;               // Reset the data register (PF7-PF0 = 00000000)
+    GPIO_PORTA_AFSEL_R |= 0x3C;             // Enable alternate functions on PA2-PA5
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFF0000FF) | 0x00222200;
+    GPIO_PORTA_DEN_R |= 0x3C;               // Enable digital on PA2-PA5
+    GPIO_PORTA_DIR_R |= 0x2C;               // Set PA2, PA3, PA5 as outputs
+    GPIO_PORTA_DIR_R &= ~0x10;              // Set PA4 as input (MISO)
 
+    SSI0_CR1_R &= ~SSI_CR1_SSE;             // Disable SSI0 during configuration
+    SSI0_CR1_R = 0x0;                       // Master mode
+    SSI0_CC_R = 0x0;                        // System clock
+    SSI0_CPSR_R = 0x10;                     // Clock prescale (16)
+    SSI0_CR0_R = SSI_CR0_DSS_8 |            // 8-bit data size
+                 SSI_CR0_FRF_MOTO |         // Freescale SPI frame format
+                 SSI_CR0_SPH | SSI_CR0_SPO; // SPI mode
+    SSI0_CR1_R |= SSI_CR1_SSE;              // Enable SSI0
 }
 
-// SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
-
-void SPI_0_Initialisation(void){
-
-    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;  // Enable clock for SPI(0) module
-    while ((SYSCTL_PRSSI_R & SYSCTL_PRSSI_R0) == 0)          // Allow time for clock to stabilize
-    GPIO_PORTA_AFSEL_R |= 0x3c;              // Alternate function,1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_PCTL_R |= (GPIO_PORTA_PCTL_R & 0xff0000ff) | 0x00222200; // Set PA2, PA3, PA4 and PA5 to SPI (0)
-    SSI0_CR1_R = 0x00;                      // SPI control, disable fully
-    SSI0_CC_R = 0x00;                       // Clock configuration - system clock
-    SSI0_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
-    SSI2_CR0_R = 0x707;                     // 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-0, SPO-0, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    //SSI0_CR0_R = 0x07c7;                    // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    SSI0_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
-
-}
-
-// SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
-
-void SPI_2_Initialisation(void){
-
-    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R2;  // Enable clock for SPI(2) module
-    while ((SYSCTL_PRSSI_R & SYSCTL_PRSSI_R2) == 0)          // Allow time for clock to stabilize
-    GPIO_PORTB_AFSEL_R |= 0xf0;              // Alternate function,1-enable (PB7-PB0 = 11110000)
-    GPIO_PORTB_PCTL_R |= (GPIO_PORTB_PCTL_R & 0x0000ffff) | 0x22220000; // Set PB4, PB5, PB6 and PB7 to SPI (2)
-    SSI2_CR1_R = 0x00;                      // SPI control, disable fully
-    SSI2_CC_R = 0x00;                       // Clock configuration - system clock
-    SSI2_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
-    SSI2_CR0_R = 0x1307;
-    //SSI2_CR0_R = 0x707;                     // working; 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-0, SPO-0, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    //SSI2_CR0_R = 0x747;                   // 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-0, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    SSI2_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
-
-}
-
-// Configure PA0 (RX) and PA1 (TX)
-
-void UART0_Initialisation(void) {
-
-    SYSCTL_RCGCUART_R |= SYSCTL_RCGC1_UART0;     // Enable UART0 clock
-    while ((SYSCTL_PRUART_R & SYSCTL_PRUART_R0) == 0)          // Allow time for clock to stabilize
-
-    GPIO_PORTA_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port A
-    GPIO_PORTA_CR_R |= 0x03;                 // Commit changes,1-enable (PA7-PA0 = 00000011)
-    GPIO_PORTA_DEN_R |= 0x03;                // Digital enable PA0 and PA1
-    GPIO_PORTA_AFSEL_R |= 0x03;              // Enable alternate function for PA0 and PA1
-    GPIO_PORTA_AMSEL_R |= 0x00;              // Turnoff analog function
-    GPIO_PORTA_PCTL_R |= 0xFFFFFF11;         // Set PCTL for PA0 and PA1
-
-    // Configure UART0 for 9600 baud rate, 8 data bits, odd parity
-    UART0_CTL_R = 0x00;                     // Disable UART before configuration
-    UART0_IBRD_R = 104;                     // Integer part of BRD = 16MHz / (16 * 9600) = 104
-    UART0_FBRD_R = 11;                      // Fractional part of BRD = 0.16 * 64 + 0.5 = 11
-    UART0_CC_R = 0x00;                      // UART clock set to system clock
-    UART0_LCRH_R = 0x60;                    // 8 bits, odd parity
-    //UART0_LCRH_R = 0x72;                    // 8 bits, odd parity
-    UART0_CTL_R = 0x301;                    // Enable UART
+// SPI transfer function
+uint8_t SPI_transfer(uint8_t data) {
+    SSI0_DR_R = data;                      // Send data
+    while (SSI0_SR_R & SSI_SR_BSY);        // Wait for transmission complete
+    return (uint8_t)(SSI0_DR_R & 0xFF);    // Return received data
 }
--- main.c
+++ main.c
@@ -1,9 +1,205 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include "inc/hw_memap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/ssi.h"
+#include "driverlib/timer.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/interrupt.h"
+#include "utils/uartstdio.h"
+#include "MFRC522.h"
 
-
-/**
- * main.c
+/*PIN Connections:
+ * Used SSI2 (Module 2)
+ *
+ * To use another Module or Reset Pin, the variables...
+ * ...NRSTPD and chipSelectPin should be changed to the Pin Mask.
+ *
+ * Also, in MFRC552_cpp, the definitions of CHIPSELECT_BASE,...
+ * ...NRSTPD_BASE and SSI_BASE must be changed to the respective...
+ * ...Port Base used.
+ *
+ * Finally, the respective chipSelectPin and NRSTPD...
+ * ... GPIO Base and Pin should be enabled in InitSSI function.
+ *
+ *
+ * Further versions should auto-change this values.
+ *
+ *
+ * SDA / CS / FSS ------------ PB5
+ * SCK  / CLK     ------------ PB4
+ * MOSI / TX      ------------ PB7
+ * MISO /  RX     ------------ PB6
+ *
+ * RST            ------------ PF0 *
+ *
  */
+
+#define redLED   0x00000002
+#define blueLED  0x00000004
+#define greenLED 0x00000008
+
+void initLeds();
+void dumpHex(unsigned char* buffer, int len);
+
+uint8_t Version;
+uint8_t AntennaGain;
+uint8_t status;
+uint32_t readTeste;
+unsigned char str[MAX_LEN];
+unsigned char cardID[CARD_LENGTH];
+unsigned char RxData[MAX_LEN];
+unsigned char TxData[MAX_LEN];
+
+
+void InitConsole(void)
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTStdioConfig(0, 115200, 16000000);
+}
+
+
+void InitSSI(){
+    uint32_t junkAuxVar;
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI2);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); //SDA
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //reset
+
+    GPIOPinConfigure(GPIO_PB4_SSI2CLK);
+    GPIOPinConfigure(GPIO_PB5_SSI2FSS);
+    GPIOPinConfigure(GPIO_PB6_SSI2RX);
+    GPIOPinConfigure(GPIO_PB7_SSI2TX);
+
+    GPIOPinTypeSSI(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_7);
+
+    GPIOPinTypeGPIOOutput(CHIP_SELECT_BASE, CHIP_SELECT_PIN); //chipSelectPin
+    GPIOPinTypeGPIOOutput(NRSTPD_BASE, NRSTPD_PIN); //NRSTPD
+
+    SSIConfigSetExpClk(SSI2_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 4000000, 8);
+
+    SSIEnable(SSI2_BASE);
+
+    while(SSIDataGetNonBlocking(SSI2_BASE, &junkAuxVar));
+
+    UARTprintf("SSI Enabled! SPI Mode!  \nData: 8bits.\n\n");
+
+}
+
+
 int main(void)
 {
-	return 0;
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); //80MHz
+
+    InitConsole();
+    initLeds();
+
+    InitSSI();
+
+    unsigned char key[6], i;
+    for (i = 0; i < 6; i++)
+        key[i] = 0xFF;
+
+    GPIOPinWrite(CHIP_SELECT_BASE, CHIP_SELECT_PIN, 0);
+    GPIOPinWrite(NRSTPD_BASE, NRSTPD_PIN, NRSTPD_PIN);
+
+    MFRC552_Init();
+
+    Version = MFRC552_ReadReg(VersionReg);
+    AntennaGain = MFRC552_ReadReg(PICC_REQIDL) & (0x70);
+
+    UARTprintf("Version: 0x%x \n", Version);
+    UARTprintf("Antenna Gain: 0x%x \n\n", AntennaGain);
+
+    while(1)
+    {
+        status = MFRC552_Request(PICC_REQIDL, str);
+        if(status == MI_OK)
+        {
+            UARTprintf("Cartao 0x%0X Detectado! \n", (str[0] << 8) + str[1]); //Card Detected
+            status = MFRC552_Anticoll(str);
+            if(status == MI_OK)
+            {
+                memcpy(cardID, str, CARD_LENGTH);
+                UARTprintf("ID: ");
+                dumpHex((unsigned char*)cardID, CARD_LENGTH);
+                if(cardID[0] == 0x36 || cardID[1] == 0xE2 || cardID[2] == 0x45 ||
+                        cardID[3] == 0x43 || cardID[0] == 0xD2)
+                    GPIOPinWrite(GPIO_PORTF_BASE, greenLED, greenLED);
+                else
+                    GPIOPinWrite(GPIO_PORTF_BASE, blueLED, blueLED);
+
+                strcpy(TxData, "1234567890");
+
+                status = MFRC552_Auth(0x60, 1, key, cardID);
+                if(status == MI_ERR)
+                {
+                    UARTprintf("FALHA NA AUTENTICACAO: 0x%X\n", MFRC552_ReadReg(ErrorReg));
+                    GPIOPinWrite(GPIO_PORTF_BASE, redLED, redLED);
+                }
+                status = MFRC552_WriteBlock(1, TxData);
+                if(status == MI_ERR)
+                {
+                    UARTprintf("FALHA NA ESCRITA: 0x%X\n", MFRC552_ReadReg(ErrorReg));
+                    GPIOPinWrite(GPIO_PORTF_BASE, redLED, redLED);
+                }
+
+                SysCtlDelay(SysCtlClockGet()/6); //Delay
+
+                status = MFRC552_Auth(0x60, 1, key, cardID);
+                if(status == MI_ERR)
+                {
+                    UARTprintf("FALHA NA AUTENTICACAO: 0x%X\n", MFRC552_ReadReg(ErrorReg));
+                    GPIOPinWrite(GPIO_PORTF_BASE, redLED, redLED);
+                }
+                status = MFRC552_ReadBlock(1, RxData);
+                if(status == MI_ERR)
+                {
+                    UARTprintf("FALHA NA LEITURA: 0x%X\n", MFRC552_ReadReg(ErrorReg));
+                    GPIOPinWrite(GPIO_PORTF_BASE, redLED, redLED);
+                }
+                else if(status == MI_OK)
+                    dumpHex((unsigned char*)RxData, 10);
+            }
+
+            else if(status == MI_ERR)
+            {
+                UARTprintf("Colisao detectada!!!\n", MFRC552_ReadReg(ErrorReg));
+                GPIOPinWrite(GPIO_PORTF_BASE, redLED, redLED);
+            }
+
+            SysCtlDelay(SysCtlClockGet()/3); //Delay
+            GPIOPinWrite(GPIO_PORTF_BASE, 0x0E, 0);
+            UARTprintf("\n\nAproxime o cartao/tag\n");
+        }
+    }
+}
+
+void initLeds(){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+}
+
+void dumpHex(unsigned char* buffer, int len){
+    int i;
+
+    for(i=0; i < len; i++)
+        UARTprintf("0x%X ", buffer[i]);
+    UARTprintf("\n\n");
 }
